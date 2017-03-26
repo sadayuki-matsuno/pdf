96d82c9404dfc1a0e8ad987d5c1611295fe8187a
Sadayuki Matsuno <sadayuki.matsuno@gmail.com>
2017-03-26 09:26:52 +0900

    fix url subdirecotry

--- config/environments.config.js
+++ config/environments.config.js
@@ -9,14 +9,14 @@ module.exports = {
   // are served webpack by to fix this issue:
   // http://stackoverflow.com/questions/34133808/webpack-ots-parsing-error-loading-fonts/34133809#34133809
   development : (config) => ({
-    compiler_public_path : `http://${config.server_host}:${config.server_port}/`
+    compiler_public_path : `http://${config.server_host}:${config.server_port}/pdf`
   }),
 
   // ======================================================
   // Overrides when NODE_ENV === 'production'
   // ======================================================
   production : (config) => ({
-    compiler_public_path     : '/',
+    compiler_public_path     : '/pdf',
     compiler_fail_on_warning : false,
     compiler_hash_type       : 'chunkhash',
     compiler_devtool         : null,
